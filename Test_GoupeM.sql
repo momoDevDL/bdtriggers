5b101b48ae6b04ce406e5fb5bdec583bc161f862
momoman852 <mohamed-masbahaboulaich@outlook.com>
2019-10-30 17:21:46 +0100

    momo n'est pas du bonne humeur aujd

--- Test_GoupeM.sql
+++ Test_GoupeM.sql
@@ -8,35 +8,9 @@ Nom du groupe : M
 
 /* Test des triggers */
 
-/* Insertion d'un pilote avec un salaire égal à 0*/
-INSERT INTO PILOTE VALUES (4, "DUJARDIN","LYON",0);
-
-/* Affichage pour vérifier que l'erreur a bien été sauvegardé dans le log */
-
-SELECT * FROM LOGERROR ;
-
-/* Insertion d'un pilote avec un salaire égal différent de 0*/
-
-INSERT INTO PILOTE VALUES (4, "DUJARDIN","LYON",10);
-
-SELECT * FROM PILOTE;
-
-/* Insertion d'un vol avec un avion non localisé dans ville de départ du vol */
-INSERT INTO VOL VALUES (12, 1, 100, "LYON", "MONTPELLIER",13,14);
-
-/* Affichage pour vérifier que l'erreur a bien été sauvegardé dans le log */
-
-SELECT * FROM LOGERROR ;
-
-/* Insertion d'un vol avec un avion  localisé dans ville de départ du vol */
-INSERT INTO VOL VALUES (12,1,100,"PARIS","MARSEILLE",22,23);
-
-
-SELECT * FROM VOL;
 
 
 /* Test fonction */
 
-SELECT NUMPIL, NOMPIL, SAL,NIVEAU_SALAIRE(SAL)
-FROM PILOTE
-ORDER BY NOMPIL;
+SELECT *,TAILLE_EVENEMENT(E_ID) FROM EVENEMENTS ;
+SELECT *,NOMBRE_PLACE_RESTANTE(E_ID) FROM EVENEMENTS ;
